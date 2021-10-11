092e696d0c420b19d0cc5ef8255b18fd316f921f
Konstantin Boryagin <nokss89@gmail.com>
2021-10-11 11:23:47 +0300

    fixed wrong directory path

---
+++ src/main/java/ru/otus/numberInWords/Main.java
@@ -0,0 +1,15 @@
+package ru.otus.numberInWords;
+
+import ru.otus.numberInWords.logic.Program;
+
+
+/**
+ * @author Konstantin Boryagin
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Program program = new Program();
+        program.startProgram();
+    }
+}
